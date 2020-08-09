bdc18346095e7bafb1e9ca73452d2d2957fddb09
chaow porkaew <pchaowmobile@gmail.com>
2020-08-09 11:54:46 +0700

    update edit forms

--- activity/views.py
+++ activity/views.py
@@ -4,21 +4,29 @@ from django.shortcuts import render
 from .forms import ActivityForm
 from .models import Activity
 
+
 # Create your views here.
 def index(request):
+    editActivity = None
+    if 'editId' in request.GET:
+        editActivity = Activity.objects.get(id=request.GET['editId'])
 
-    if request.POST :
-        newAct = Activity()
-        newAct.activity_name = request.POST['activity_name']
-        newAct.activity_type = request.POST['activity_type']
-        newAct.activity_desc = request.POST['activity_desc']
-        newAct.save()
+    if request.POST:
+        createForm = ActivityForm(request.POST,instance=editActivity)
+        if (createForm.is_valid()):
+            newAct = createForm.save(commit=False)
+            newAct.save()
+        editActivity = None
 
     activities = Activity.objects.all()
-    activityForm = ActivityForm()
+    if editActivity :
+        activityForm = ActivityForm(instance=editActivity)
+    else :
+        activityForm = ActivityForm()
 
-    return render(request,"activity/index.html",{
-        'title' : "Hello From Title",
-        'activities' : activities,
-        "activityForm" : activityForm
+    return render(request, "activity/index.html", {
+        'title': "Hello From Title",
+        'activities': activities,
+        "activityForm": activityForm,
+        'editActivity' : editActivity
     })
