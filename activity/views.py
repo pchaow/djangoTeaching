e854950ea96c584bcb2e5364cc06d4ddf6a6709d
chaow porkaew <pchaowmobile@gmail.com>
2020-08-09 11:16:11 +0700

    add forms

--- activity/views.py
+++ activity/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ActivityForm
+from .models import Activity
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello")
+
+    if request.POST :
+        newAct = Activity()
+        newAct.activity_name = request.POST['activity_name']
+        newAct.activity_type = request.POST['activity_type']
+        newAct.activity_desc = request.POST['activity_desc']
+        newAct.save()
+
+    activities = Activity.objects.all()
+    activityForm = ActivityForm()
+
+    return render(request,"activity/index.html",{
+        'title' : "Hello From Title",
+        'activities' : activities,
+        "activityForm" : activityForm
+    })
