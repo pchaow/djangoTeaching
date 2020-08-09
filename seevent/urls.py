77ed37f77a3c66a98cdd7069a559e6c0485ccfb3
chaow porkaew <pchaowmobile@gmail.com>
2020-08-09 10:32:50 +0700

    add first view

--- seevent/urls.py
+++ seevent/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from activity import views as activityViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', activityViews.index),
 ]
